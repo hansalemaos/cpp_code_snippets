32a04df11923e05ca17e9c3342ba11cd77ec667b
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2025-01-19 18:34:44 -0300

    Create replace_charset_with_char.cpp

---
+++ replace_charset_with_char.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+static void constexpr replace_charset_with_char(std::string &s, const std::string_view chars, char c = ' ')
+{
+    if (chars.empty() || s.empty())
+    {
+        return;
+    }
+    for (size_t i{}; i < s.size(); i++)
+    {
+        for (size_t j{}; j < chars.size(); j++)
+        {
+            if (s[i] == chars[j])
+            {
+                s[i] = c;
+                break;
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::string mystring{"Hello XXAXB World!"};
+
+    std::cout << mystring << std::endl;
+    replace_charset_with_char(mystring, "XA !", 'Q');
+    std::cout << mystring << std::endl;
+
+    return 0;
+}
