832714f5c8d27c4fd6140bcdf65272e70fa91a48
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2025-01-19 19:01:48 -0300

    Create is_rectangle_in_rectangle.cpp

---
+++ is_rectangle_in_rectangle.cpp
@@ -0,0 +1,20 @@
+#include <array>
+#include <cstdint>
+#include <iostream>
+#include <ranges>
+#include <string>
+#include <string_view>
+#include <vector>
+
+bool static constexpr is_rect_in_rect(const std::array<int64_t, 4> &rect1, const std::array<int64_t, 4> &rect2)
+{
+
+    return (rect1[0] >= rect2[0] && rect1[1] >= rect2[1] && rect1[2] <= rect2[2] && rect1[3] <= rect2[3]);
+}
+int main()
+{
+    std::cout << is_rect_in_rect({1, 2, 3, 4}, {1, 2, 3, 4}) << std::endl;
+    std::cout << is_rect_in_rect({1, 2, 3, 4}, {1, 2, 30, 40}) << std::endl;
+    std::cout << is_rect_in_rect({0, 0, 300, 400}, {1, 2, 30, 40}) << std::endl;
+    return 0;
+}
