aaaee4161ff666239c7c3a76b39708937eac0bad
Hans Alemão <aulasparticularesdealemaosp@gmail.com>
2025-01-22 23:05:39 -0300

    Create split_string_at_whitespaces.cpp

---
+++ split_string_at_whitespaces.cpp
@@ -0,0 +1,59 @@
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+std::vector<std::string> split_string_at_whitespace(const std::string_view str, int maxsplit)
+{
+    std::vector<std::string> result;
+    std::string::size_type i;
+    std::string::size_type j;
+    std::string::size_type len = str.size();
+    for (i = j = 0; i < len;)
+    {
+        while (i < len && ::isspace(str[i]))
+        {
+            i++;
+        }
+        j = i;
+        while (i < len && !::isspace(str[i]))
+        {
+            i++;
+        }
+        if (j < i)
+        {
+            if (maxsplit-- <= 0)
+            {
+                break;
+            }
+            result.emplace_back(std::string(str.substr(j, i - j)));
+            while (i < len && ::isspace(str[i]))
+            {
+                i++;
+            }
+            j = i;
+        }
+    }
+    if (j < len)
+    {
+        result.emplace_back(std::string(str.substr(j, len - j)));
+    }
+    return result;
+}
+
+int main()
+{
+    std::string mystring{"Hello, World!  xx"};
+    auto vec1{split_string_at_whitespace(mystring, 1)};
+    auto vec2{split_string_at_whitespace(mystring, 100000)};
+    for (const auto &s : vec1)
+    {
+        std::cout << s << std::endl;
+    }
+    for (const auto &s : vec2)
+    {
+        std::cout << s << std::endl;
+    }
+    return 0;
+}
